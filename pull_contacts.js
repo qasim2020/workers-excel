8debc719bcbc236e62c35d8f97632272ef29f1f6
Qasim Ali <qasimali24@gmail.com>
2022-10-18 19:16:35 +0500

    pulled the places info through placeIds - now push the data to airtable (may be write code in modules as a separate file)

--- pull_contacts.js
+++ pull_contacts.js
@@ -1,9 +1,13 @@
-console.log(" pull google details address, contacts, phone numbers etc to local computer in a text file");
+// pull google details address, contacts, phone numbers etc to local computer in a text file
+
+console.log(" — APP STARTED : Pull google details address, contacts, phone numbers etc to local computer in a text file");
 
 const readXlsxFile = require('read-excel-file/node');
 const fs = require('fs');
 const axios = require('axios');
+const { similarity, editDistance } = require("./similarity.js");
 var env = process.env.NODE_ENV || 'development';
+
 
 if (env === 'development' || env === 'test') {
   var config = require('./config.json');
@@ -14,13 +18,14 @@ if (env === 'development' || env === 'test') {
 }
 
 // /Users/qasim/NodeJs/workers-excel/airtable_sample.xlsx
-
+// airtable_sample_small.xlsx
 let testFiles = "";
 
 fullProcedure = function() {
-    readXlsxFile('airtable_sample.xlsx').then( rows => {
+    readXlsxFile('airtable_sample_small.xlsx').then( rows => {
+
         rows.shift();
-        console.log(rows);
+        console.log(`— DATA has ${rows.length} Rows`);
         let obj = rows.map( val => {
             return {
                 string: val[0] + " " + val[3],
@@ -28,12 +33,10 @@ fullProcedure = function() {
             }
         });
 
+        let placeId = [];
         obj.forEach( (val, index)  => {
 
-            if (index > 1) return console.log("APP ENDED -- HERE AFTER RUN ONCE SURE");
-            console.log({index});
-
-            console.log( val.string );
+            console.log(`- INDEX No ${index} is going to be fetched from PLACES API`);
 
             let config = {
                 method: 'get',
@@ -43,19 +46,34 @@ fullProcedure = function() {
 
             axios(config)
             .then(function (response) {
-                console.log(JSON.stringify(response.data, 0, 2));
+                // console.log(JSON.stringify(response.data, 0, 2));
+
                 Object.assign( val , {
-                    api_data_1 : JSON.stringify(response.data)
+                    api1 : response.data
                 });
-                fs.writeFile(
-                    'place_id.json', 
-                    JSON.stringify( val , 0 , 2 ) , 
-                    (err) => {
-                        if (err) {
-                          return console.log('Failed to backup');
-                        }
-                        return console.log('SUCCESS ::::  created place_id.json');
-                    });
+
+                placeId.push(val);
+                console.log( `— Pulled ${placeId.length} out of ${obj.length} `); 
+
+                if (placeId.length == obj.length) {
+
+                    console.log("— Job Completed now write the data inside the file");
+                    // return console.log(placeId);
+                    fs.writeFile(
+                        'place_id.json', 
+                        JSON.stringify( placeId , 0 , 2 ) , 
+                        (err) => {
+                            if (err) {
+                              return console.log('!!!!! — failed to backup');
+                            }
+                            return console.log('— success , created place_id.json');
+                        });
+
+                };
+
+                return;
+                
+                // if job done here write else wait until all indexes are updated
             })
             .catch(function (error) {
                 console.log(error);
@@ -72,11 +90,100 @@ fullProcedure = function() {
 
 let fetchPlaceInfo = function() {
 
-    fs.readFile(`place_id.json`, 'utf8', (err, data) => {
+    fs.readFile(`place_id.json`, 'utf8', (err, input) => {
+
         if (err) {
             return console.log(err);
         }
-        console.log( data );
+
+        let placeId = [],
+            data = JSON.parse(input);
+
+        data.forEach( (val, index) => {
+
+            console.log(`- INDEX No ${index} is going to be fetched from PLACES API`);
+
+            let matchIndex = [];
+
+            console.log(val);
+
+            val.api1.candidates.forEach( val1 => {
+
+                let match = similarity(val.string, val1.name + " " + val1.formatted_address);
+
+                Object.assign(val1, {
+                    match: match
+                });
+
+                matchIndex.push(match);
+
+            });
+
+            let max = Math.max( ...matchIndex );
+
+            console.log( max );
+
+            val.api1 = val.api1.candidates.find( val1 => val1.match == max );
+
+            let fields = [
+                'name',
+                'formatted_address', 
+                'formatted_phone_number',
+                'international_phone_number',
+                'name', 
+                'photos', 
+                'price_level', 
+                'rating', 
+                'reviews', 
+                'url', 
+                'user_ratings_total', 
+                'utc_offset', 
+                'website'
+            ];
+
+            fields = fields.join("%2C")
+
+            console.log(" — Pull place details through place Ids");
+
+            let config = {
+                method: 'get',
+                url: `https://maps.googleapis.com/maps/api/place/details/json?place_id=${val.api1.place_id}&fields=${fields}&key=${process.env.places}`, 
+                headers: { }
+            };
+
+            axios(config)
+            .then(function (response) {
+                console.log(JSON.stringify(response.data, 0, 2));
+                Object.assign( val, {
+                    api2: response.data
+                });
+                placeId.push(val);
+                console.log( `— Pulled ${placeId.length} out of ${data.length} `); 
+                if (placeId.length == data.length) {
+
+                    console.log("— Job Completed now write the data inside the file");
+                    fs.writeFile(
+                        'place_full_info.json', 
+                        JSON.stringify( placeId , 0 , 2 ) , 
+                        (err) => {
+                            if (err) {
+                              return console.log('!!!!! — failed to backup');
+                            }
+                            return console.log('— SUCCESS , created place_full_info.json');
+                        }
+                    );
+
+                };
+
+                return;
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+
+
+        });
+
     });
 
 };
@@ -89,12 +196,13 @@ fs.readdir("./", (err, files) => {
     console.log( testFiles );
 
     switch (true) {
+        case (testFiles.place_full_info) :
+            console.log("push the data to airtable");
+            // TODO: Sift the data according to airtable requirement - and push all of it into airtable test file - pull all of CSV from airtable - push all of it through the script back to the Airtable
+            break;
         case (testFiles.place_id) : 
             console.log("fetch the place_full_info");
             fetchPlaceInfo();
-            break;
-        case (testFiles.place_full_info) :
-            console.log("push the data to airtable");
             break;
         default: 
             console.log("fetch google from sample - create place_id.json - create place_full_info.json - push the data to airtable");
